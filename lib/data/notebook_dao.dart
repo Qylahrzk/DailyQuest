19e022aee54620547e768b0b6f4e5d6811cd9c3e
Qylahrzk <nuraqilahnur@gmail.com>
2025-07-04 03:31:00 +0800

    Update Notebook_Dao

--- lib/data/notebook_dao.dart
+++ lib/data/notebook_dao.dart
@@ -1,41 +1,21 @@
+// ignore: unused_import
 import 'package:sqflite/sqflite.dart';
-import 'package:path/path.dart';
+import 'app_database.dart';
 
+/// ✅ Data Access Object for Notebooks
 class NotebookDao {
-  static Database? _db;
-
-  static Future<Database> get database async {
-    if (_db != null) return _db!;
-    _db = await openDatabase(
-      join(await getDatabasesPath(), 'diary.db'),
-      onCreate: (db, version) {
-        db.execute('''
-          CREATE TABLE notebooks(
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            title TEXT,
-            color INTEGER
-          )
-        ''');
-      },
-      version: 1,
-    );
-    return _db!;
+  /// 🔹 Get all notebooks
+  static Future<List<Map<String, dynamic>>> getAll() async {
+    final db = await AppDatabase.database;
+    return await db.query('notebooks');
   }
 
-  static Future<List<Map<String, dynamic>>> getAllNotebooks() async {
-    final db = await database;
-    return db.query('notebooks');
-  }
-
-  static Future<int> insertNotebook(String title, int color) async {
-    final db = await database;
-    return db.insert('notebooks', {
+  /// 🔹 Insert new notebook
+  static Future<int> insert(String title, int color) async {
+    final db = await AppDatabase.database;
+    return await db.insert('notebooks', {
       'title': title,
       'color': color,
     });
   }
-
-  static insert(String trim, int value) {}
-
-  static getAll() {}
 }
